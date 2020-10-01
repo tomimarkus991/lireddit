dc60c1bc57d03c4670983648fe8c0aea7e19411e
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-10-01 12:45:48 +0300

    Apollo Server Express Setup

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 #lireddit
+
+login: psql -U postgres
+pass: postgres
+
+create database lireddit;
+\list
+
+create migrations: npx mikro-orm migration:create
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import { MikroORM } from "@mikro-orm/core";
 // import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig); // conntect to db
   await orm.getMigrator().up(); // run migrations
 
-  //   const post = orm.em.create(Post, { title: "my first post" });
-  //   await orm.em.persistAndFlush(post);
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  apolloServer.applyMiddleware({ app });
 
-  //   const posts = await orm.em.find(Post, {});
-  //   console.log(posts);
+  app.listen(3000, () => console.log("server started on loclahost:3000"));
 };
 
 main().catch((err) => console.error(err));
