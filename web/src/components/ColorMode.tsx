8345d4ee3149012470e0655bdba976268edff089
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-10-08 16:04:26 +0300

    make navbar style better

--- web/src/components/ColorMode.tsx
+++ web/src/components/ColorMode.tsx
@@ -7,7 +7,6 @@ const ColorMode: React.FC<ColorModeProps> = ({}) => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <IconButton
-      mb={4}
       icon={colorMode === "light" ? "moon" : "sun"}
       onClick={toggleColorMode}
       aria-label="Switch color-mode"
--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Button, Flex, Link, Text } from "@chakra-ui/core";
+import { Box, Button, Flex, Link, Text, useColorMode } from "@chakra-ui/core";
 import NextLink from "next/link";
 import { useMeQuery } from "../generated/graphql";
 import ColorMode from "./ColorMode";
@@ -8,6 +8,9 @@ interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
   const [{ data, fetching }] = useMeQuery();
+  const { colorMode } = useColorMode();
+  const bgColor = { light: "#F7FAFC", dark: "#2D3748" };
+  const color = { light: "#242526", dark: "#E4E6EB" };
   let body;
   // data is loading
   if (fetching) {
@@ -16,7 +19,7 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
   // user not logged in
   else if (!data?.me) {
     body = (
-      <>
+      <Box ml="auto">
         {" "}
         <NextLink href="login">
           <Link mr={2}>Login</Link>
@@ -24,22 +27,28 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
         <NextLink href="register">
           <Link>Register</Link>
         </NextLink>
-      </>
+      </Box>
     );
   }
   // user logged in
   else {
     body = (
-      <Flex>
-        <Text mr={2}>{data.me.username}</Text>
-        <Button variant="link">Logout</Button>
-      </Flex>
+      <Box mr="auto">
+        <Flex>
+          <Text color={color[colorMode]} mr={2}>
+            {data.me.username}
+          </Text>
+          <Button color={color[colorMode]} variant="link">
+            Logout
+          </Button>
+        </Flex>
+      </Box>
     );
   }
   return (
-    <Flex bg="tomato" p={4}>
+    <Flex bg={bgColor[colorMode]} p={3} align="center">
+      {body}
       <ColorMode />
-      <Box ml="auto">{body}</Box>
     </Flex>
   );
 };
