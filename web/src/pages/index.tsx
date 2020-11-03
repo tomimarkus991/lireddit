300fe0a245c0e1b9d6f68053f22e95ca9afa66a0
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-11-03 17:43:12 +0200

    added mock data

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -8,7 +8,7 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 const Index = () => {
   const [{ data }] = usePostsQuery({
     variables: {
-      limit: 2,
+      limit: 10,
     },
   });
   return (
